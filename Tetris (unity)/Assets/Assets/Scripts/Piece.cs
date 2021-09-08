bf1b0fbe002dd16b7c2b81781578e44c83ec57b6
Roman <rom.vorobyov@gmail.com>
2021-09-08 18:56:22 +0300

    New version
    Add new sprites
    Add spawning (not working)

---
+++ Tetris (unity)/Assets/Assets/Scripts/Piece.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public Board board { get; private set; }
+    public TetrominoData data { get; private set; }
+    public Vector3Int[] cells { get; private set; }
+    public Vector3Int position { get; private set; }
+
+    public void Initialize(Board board, TetrominoData data, Vector3Int position)
+    {
+        this.board = board;
+        this.data = data;
+        this.position = position;
+        
+        if (this.cells == null)
+        {
+            this.cells = new Vector3Int[data.cells.Length];
+        }
+
+        for (int i = 0; i < data.cells.Length; i++)
+        {
+            this.cells[i] = (Vector3Int)data.cells[i];
+        }
+    }
+}
