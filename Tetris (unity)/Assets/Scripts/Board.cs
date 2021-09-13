44871f395217a27a5734a3c6f20a73e09f77a2a5
Roman <rom.vorobyov@gmail.com>
2021-09-13 04:36:12 +0300

    Fixed spawning and added movement.

--- Tetris (unity)/Assets/Scripts/Board.cs
+++ Tetris (unity)/Assets/Scripts/Board.cs
@@ -6,7 +6,17 @@ public class Board : MonoBehaviour
     public Tilemap tilemap { get; private set; }
     public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominoes;
-    public Vector3Int spawnPosition;
+    public Vector3Int spawn_pos;
+    public Vector2Int board_size = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-this.board_size.x / 2, -this.board_size.y / 2);
+            return new RectInt(position, this.board_size);
+        }
+    }
 
     private void Awake()
     {
@@ -29,7 +39,7 @@ public class Board : MonoBehaviour
         int random = Random.Range(0, this.tetrominoes.Length);
         TetrominoData data = this.tetrominoes[random];
 
-        this.activePiece.Initialize(this, data, this.spawnPosition);
+        this.activePiece.Initialize(this, data, this.spawn_pos);
         Set(this.activePiece);
     }
 
@@ -37,8 +47,38 @@ public class Board : MonoBehaviour
     {
         for (int i = 0; i < piece.cells.Length; i++)
         {
-            Vector3Int tilePosition = piece.cells[i] + piece.position;
-            this.tilemap.SetTile(tilePosition, piece.data.tile);
+            Vector3Int tile_pos = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tile_pos, piece.data.tile);
         }
+    }
+
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tile_pos = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tile_pos, null);
+        }
+    }
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = this.Bounds;
+
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tile_pos= piece.cells[i] + piece.position;
+
+            if (!bounds.Contains((Vector2Int)tile_pos))
+            {
+                return false;
+            }
+
+            if (this.tilemap.HasTile(tile_pos))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- Tetris (unity)/Assets/Scripts/Piece.cs
+++ Tetris (unity)/Assets/Scripts/Piece.cs
@@ -23,4 +23,54 @@ public class Piece : MonoBehaviour
             this.cells[i] = (Vector3Int)data.cells[i];
         }
     }
+
+    private void Update()
+    {
+        this.board.Clear(this);
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(Vector2Int.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(Vector2Int.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Move(Vector2Int.down);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HardDrop();
+        }
+
+        this.board.Set(this);
+    }
+
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
+    }
+
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int new_pos = this.position;
+        new_pos.x += translation.x;
+        new_pos.y += translation.y;
+
+        bool valid = this.board.IsValidPosition(this, new_pos);
+
+        if (valid)
+        {
+            this.position = new_pos;
+        }
+
+        return valid;
+    }
 }
